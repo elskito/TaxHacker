7e4967ce070a30ff0206946dbfe68a2def5d6264
tomek <tomasz.lawski@outlook.com>
2026-02-02 18:32:55 +0100

    Simplify VAT and VAT rate migration logic in SQL script

--- prisma/migrations/20260202120000_add_vat_columns/migration.sql
+++ prisma/migrations/20260202120000_add_vat_columns/migration.sql
@@ -4,25 +4,21 @@ ALTER TABLE "transactions" ADD COLUMN "vat_rate" DOUBLE PRECISION;
 
 -- Backfill VAT data from extra JSON
 UPDATE "transactions"
-SET "vat" = ROUND((extra->>'vat')::numeric * 100)
-WHERE extra ? 'vat'
-  AND (extra->>'vat') ~ '^-?\\d+(\\.\\d+)?$';
-
-UPDATE "transactions"
-SET "vat_rate" = (extra->>'vat_rate')::double precision
-WHERE extra ? 'vat_rate'
-  AND (extra->>'vat_rate') ~ '^-?\\d+(\\.\\d+)?$';
-
--- Remove migrated keys from extra (only when values were numeric)
-UPDATE "transactions"
-SET extra = extra - 'vat'
-WHERE extra ? 'vat'
-  AND (extra->>'vat') ~ '^-?\\d+(\\.\\d+)?$';
-
-UPDATE "transactions"
-SET extra = extra - 'vat_rate'
-WHERE extra ? 'vat_rate'
-  AND (extra->>'vat_rate') ~ '^-?\\d+(\\.\\d+)?$';
+SET
+    vat = CASE
+        WHEN extra->>'vat' IS NOT NULL
+             AND extra->>'vat' <> ''
+        THEN ((extra->>'vat')::NUMERIC * 100)::INTEGER
+        ELSE vat
+    END,
+    vat_rate = CASE
+        WHEN extra->>'vat_rate' IS NOT NULL
+             AND extra->>'vat_rate' <> ''
+        THEN (extra->>'vat_rate')::DOUBLE PRECISION
+        ELSE vat_rate
+    END,
+    extra = extra - 'vat' - 'vat_rate'
+WHERE extra ?| ARRAY['vat', 'vat_rate'];
 
 -- Update field definitions to treat VAT as standard fields
 UPDATE "fields" SET "code" = 'vatRate', "is_extra" = false WHERE "code" = 'vat_rate';
